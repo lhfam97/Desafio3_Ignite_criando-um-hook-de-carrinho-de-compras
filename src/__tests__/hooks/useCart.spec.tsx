add74f1e2136f3880934463db64c4fd7a0f58a5d
Luís Henrique Machado <lhfam97@gmail.com>
2021-03-29 00:44:25 -0300

    Finalizando o desafio

--- src/__tests__/hooks/useCart.spec.tsx
+++ src/__tests__/hooks/useCart.spec.tsx
@@ -1,47 +1,47 @@
-import { renderHook, act } from '@testing-library/react-hooks';
-import AxiosMock from 'axios-mock-adapter';
+import { renderHook, act } from "@testing-library/react-hooks";
+import AxiosMock from "axios-mock-adapter";
 
-import { toast } from 'react-toastify';
-import { api } from '../../services/api';
-import { useCart, CartProvider } from '../../hooks/useCart';
+import { toast } from "react-toastify";
+import { api } from "../../services/api";
+import { useCart, CartProvider } from "../../hooks/useCart";
 
 const apiMock = new AxiosMock(api);
 
-jest.mock('react-toastify');
+jest.mock("react-toastify");
 
 const mockedToastError = toast.error as jest.Mock;
-const mockedSetItemLocalStorage = jest.spyOn(Storage.prototype, 'setItem');
+const mockedSetItemLocalStorage = jest.spyOn(Storage.prototype, "setItem");
 const initialStoragedData = [
   {
     id: 1,
     amount: 2,
     image:
-      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+      "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
     price: 179.9,
-    title: 'Tênis de Caminhada Leve Confortável',
+    title: "Tênis de Caminhada Leve Confortável"
   },
   {
     id: 2,
     amount: 1,
     image:
-      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+      "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg",
     price: 139.9,
-    title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
-  },
+    title: "Tênis VR Caminhada Confortável Detalhes Couro Masculino"
+  }
 ];
 
-describe('useCart Hook', () => {
+describe("useCart Hook", () => {
   beforeEach(() => {
     apiMock.reset();
 
     jest
-      .spyOn(Storage.prototype, 'getItem')
+      .spyOn(Storage.prototype, "getItem")
       .mockReturnValueOnce(JSON.stringify(initialStoragedData));
   });
 
-  it('should be able to initialize cart with localStorage value', () => {
+  it("should be able to initialize cart with localStorage value", () => {
     const { result } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     expect(result.current.cart).toEqual(
@@ -50,39 +50,39 @@ describe('useCart Hook', () => {
           id: 1,
           amount: 2,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
           price: 179.9,
-          title: 'Tênis de Caminhada Leve Confortável',
+          title: "Tênis de Caminhada Leve Confortável"
         },
         {
           id: 2,
           amount: 1,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg",
           price: 139.9,
-          title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
-        },
+          title: "Tênis VR Caminhada Confortável Detalhes Couro Masculino"
+        }
       ])
     );
   });
 
-  it('should be able to add a new product', async () => {
+  it("should be able to add a new product", async () => {
     const productId = 3;
 
     apiMock.onGet(`stock/${productId}`).reply(200, {
       id: 3,
-      amount: 2,
+      amount: 2
     });
     apiMock.onGet(`products/${productId}`).reply(200, {
       id: 3,
-      title: 'Tênis Adidas Duramo Lite 2.0',
+      title: "Tênis Adidas Duramo Lite 2.0",
       price: 219.9,
       image:
-        'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
+        "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg"
     });
 
     const { result, waitForNextUpdate } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
@@ -97,42 +97,42 @@ describe('useCart Hook', () => {
           id: 1,
           amount: 2,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
           price: 179.9,
-          title: 'Tênis de Caminhada Leve Confortável',
+          title: "Tênis de Caminhada Leve Confortável"
         },
         {
           id: 2,
           amount: 1,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg",
           price: 139.9,
-          title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+          title: "Tênis VR Caminhada Confortável Detalhes Couro Masculino"
         },
         {
           id: 3,
           amount: 1,
-          title: 'Tênis Adidas Duramo Lite 2.0',
+          title: "Tênis Adidas Duramo Lite 2.0",
           price: 219.9,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
-        },
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg"
+        }
       ])
     );
     expect(mockedSetItemLocalStorage).toHaveBeenCalledWith(
-      '@RocketShoes:cart',
+      "@RocketShoes:cart",
       JSON.stringify(result.current.cart)
     );
   });
 
-  it('should not be able add a product that does not exist', async () => {
+  it("should not be able add a product that does not exist", async () => {
     const productId = 4;
 
     apiMock.onGet(`stock/${productId}`).reply(404);
     apiMock.onGet(`products/${productId}`).reply(404);
 
     const { result, waitFor } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
@@ -142,7 +142,7 @@ describe('useCart Hook', () => {
     await waitFor(
       () => {
         expect(mockedToastError).toHaveBeenCalledWith(
-          'Erro na adição do produto'
+          "Erro na adição do produto"
         );
         expect(result.current.cart).toEqual(
           expect.arrayContaining(initialStoragedData)
@@ -153,23 +153,23 @@ describe('useCart Hook', () => {
     );
   });
 
-  it('should be able to increase a product amount when adding a product that already exists on cart', async () => {
+  it("should be able to increase a product amount when adding a product that already exists on cart", async () => {
     const productId = 1;
 
     apiMock.onGet(`stock/${productId}`).reply(200, {
       id: 1,
-      amount: 3,
+      amount: 3
     });
     apiMock.onGet(`products/${productId}`).reply(200, {
       id: 1,
       image:
-        'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+        "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
       price: 179.9,
-      title: 'Tênis de Caminhada Leve Confortável',
+      title: "Tênis de Caminhada Leve Confortável"
     });
 
     const { result, waitForNextUpdate } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
@@ -184,36 +184,36 @@ describe('useCart Hook', () => {
           id: 1,
           amount: 3,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
           price: 179.9,
-          title: 'Tênis de Caminhada Leve Confortável',
+          title: "Tênis de Caminhada Leve Confortável"
         },
         {
           id: 2,
           amount: 1,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg",
           price: 139.9,
-          title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
-        },
+          title: "Tênis VR Caminhada Confortável Detalhes Couro Masculino"
+        }
       ])
     );
     expect(mockedSetItemLocalStorage).toHaveBeenCalledWith(
-      '@RocketShoes:cart',
+      "@RocketShoes:cart",
       JSON.stringify(result.current.cart)
     );
   });
 
-  it('should not be able to increase a product amount when running out of stock', async () => {
+  it("should not be able to increase a product amount when running out of stock", async () => {
     const productId = 2;
 
     apiMock.onGet(`stock/${productId}`).reply(200, {
       id: 2,
-      amount: 1,
+      amount: 1
     });
 
     const { result, waitFor } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
@@ -223,7 +223,7 @@ describe('useCart Hook', () => {
     await waitFor(
       () => {
         expect(mockedToastError).toHaveBeenCalledWith(
-          'Quantidade solicitada fora de estoque'
+          "Quantidade solicitada fora de estoque"
         );
         expect(result.current.cart).toEqual(
           expect.arrayContaining(initialStoragedData)
@@ -231,16 +231,16 @@ describe('useCart Hook', () => {
         expect(mockedSetItemLocalStorage).not.toHaveBeenCalled();
       },
       {
-        timeout: 200,
+        timeout: 200
       }
     );
   });
 
-  it('should be able to remove a product', () => {
+  it("should be able to remove a product", () => {
     const productId = 2;
 
     const { result } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
@@ -253,46 +253,46 @@ describe('useCart Hook', () => {
           amount: 2,
           id: 1,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
           price: 179.9,
-          title: 'Tênis de Caminhada Leve Confortável',
-        },
+          title: "Tênis de Caminhada Leve Confortável"
+        }
       ])
     );
     expect(mockedSetItemLocalStorage).toHaveBeenCalledWith(
-      '@RocketShoes:cart',
+      "@RocketShoes:cart",
       JSON.stringify(result.current.cart)
     );
   });
 
-  it('should not be able to remove a product that does not exist', () => {
+  it("should not be able to remove a product that does not exist", () => {
     const productId = 3;
 
     const { result } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
       result.current.removeProduct(productId);
     });
 
-    expect(mockedToastError).toHaveBeenCalledWith('Erro na remoção do produto');
+    expect(mockedToastError).toHaveBeenCalledWith("Erro na remoção do produto");
     expect(result.current.cart).toEqual(
       expect.arrayContaining(initialStoragedData)
     );
     expect(mockedSetItemLocalStorage).not.toHaveBeenCalled();
   });
 
-  it('should be able to update a product amount', async () => {
+  it("should be able to update a product amount", async () => {
     const productId = 2;
 
     apiMock.onGet(`stock/${productId}`).reply(200, {
       id: 2,
-      amount: 5,
+      amount: 5
     });
 
     const { result, waitForNextUpdate } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
@@ -307,33 +307,33 @@ describe('useCart Hook', () => {
           id: 1,
           amount: 2,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
           price: 179.9,
-          title: 'Tênis de Caminhada Leve Confortável',
+          title: "Tênis de Caminhada Leve Confortável"
         },
         {
           id: 2,
           amount: 2,
           image:
-            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+            "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg",
           price: 139.9,
-          title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
-        },
+          title: "Tênis VR Caminhada Confortável Detalhes Couro Masculino"
+        }
       ])
     );
     expect(mockedSetItemLocalStorage).toHaveBeenCalledWith(
-      '@RocketShoes:cart',
+      "@RocketShoes:cart",
       JSON.stringify(result.current.cart)
     );
   });
 
-  it('should not be able to update a product that does not exist', async () => {
+  it("should not be able to update a product that does not exist", async () => {
     const productId = 4;
 
     apiMock.onGet(`stock/${productId}`).reply(404);
 
     const { result, waitFor } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
@@ -343,7 +343,7 @@ describe('useCart Hook', () => {
     await waitFor(
       () => {
         expect(mockedToastError).toHaveBeenCalledWith(
-          'Erro na alteração de quantidade do produto'
+          "Erro na alteração de quantidade do produto"
         );
         expect(result.current.cart).toEqual(
           expect.arrayContaining(initialStoragedData)
@@ -354,16 +354,16 @@ describe('useCart Hook', () => {
     );
   });
 
-  it('should not be able to update a product amount when running out of stock', async () => {
+  it("should not be able to update a product amount when running out of stock", async () => {
     const productId = 2;
 
     apiMock.onGet(`stock/${productId}`).reply(200, {
       id: 2,
-      amount: 1,
+      amount: 1
     });
 
     const { result, waitFor } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
@@ -373,7 +373,7 @@ describe('useCart Hook', () => {
     await waitFor(
       () => {
         expect(mockedToastError).toHaveBeenCalledWith(
-          'Quantidade solicitada fora de estoque'
+          "Quantidade solicitada fora de estoque"
         );
         expect(result.current.cart).toEqual(
           expect.arrayContaining(initialStoragedData)
@@ -384,16 +384,16 @@ describe('useCart Hook', () => {
     );
   });
 
-  it('should not be able to update a product amount to a value smaller than 1', async () => {
+  it("should not be able to update a product amount to a value smaller than 1", async () => {
     const productId = 2;
 
     apiMock.onGet(`stock/${productId}`).reply(200, {
       id: 2,
-      amount: 1,
+      amount: 1
     });
 
     const { result, waitForValueToChange } = renderHook(useCart, {
-      wrapper: CartProvider,
+      wrapper: CartProvider
     });
 
     act(() => {
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,39 +23,114 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const { data } = await api.get<Stock>(`stock/${productId}`);
+
+      const productExistsInCart = cart.find(
+        product => product.id === productId
+      );
+
+      const amount = productExistsInCart ? productExistsInCart.amount + 1 : 1;
+
+      if (amount > data.amount) {
+        toast.error("Quantidade solicitada fora de estoque");
+
+        return;
+      }
+      if (productExistsInCart) {
+        const newProducts = cart.map(product => {
+          if (product.id === productId) {
+            return {
+              ...product,
+              amount: product.amount + 1
+            };
+          }
+          return product;
+        });
+        setCart(newProducts);
+
+        localStorage.setItem("@RocketShoes:cart", JSON.stringify(newProducts));
+        return;
+      }
+
+      const response = await api.get<Product>(`products/${productId}`);
+      const newProducts = [
+        ...cart,
+        {
+          ...response.data,
+          amount: 1
+        }
+      ];
+      setCart(newProducts);
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(newProducts));
+    } catch (err) {
+      console.log(err);
+      toast.error("Erro na adição do produto");
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const productExistsInCart = cart.find(
+        product => product.id === productId
+      );
+      if (productExistsInCart) {
+        const productsInCart = cart.filter(product => product.id !== productId);
+
+        setCart(productsInCart);
+        localStorage.setItem(
+          "@RocketShoes:cart",
+          JSON.stringify(productsInCart)
+        );
+      } else {
+        throw new Error("Produto não encontrado no carrinho");
+      }
     } catch {
-      // TODO
+      toast.error("Erro na remoção do produto");
     }
   };
 
   const updateProductAmount = async ({
     productId,
-    amount,
+    amount
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      const { data } = await api.get<Stock>(`stock/${productId}`);
+
+      if (amount < 1) {
+        throw new Error("Alteraçáo inválida");
+      }
+
+      if (amount > data.amount) {
+        toast.error("Quantidade solicitada fora de estoque");
+
+        return;
+      }
+      const newProducts = cart.map(product => {
+        if (productId === product.id) {
+          return {
+            ...product,
+            amount: amount
+          };
+        }
+        return product;
+      });
+      setCart(newProducts);
+
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(newProducts));
+      return;
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
